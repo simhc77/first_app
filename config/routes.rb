d928ec374330175494c48398e907f12a664014af
simhc77 <alex.simhc@gmail.com>
2011-08-06 11:49:15 +0900

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 FirstApp::Application.routes.draw do
-  get "page/home"
-
-  get "page/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,10 +50,13 @@ FirstApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => 'page#home'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  match '/contact', :to => 'page#contact'
+  match '/home',     :to => 'page#home'
 end
