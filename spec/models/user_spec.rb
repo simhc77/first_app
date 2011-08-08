02cd6441d01d5f58b7c7e04f2b1bba4cb7fd9aa5
simhc77 <alex.simhc@gmail.com>
2011-08-08 22:43:19 +0900

    CH6

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+
+describe User do
+  before :each do
+    @attr = { :name => "example", :email => "user@a.b.c" }
+  end
+
+  it "should create a new instance given valid att." do
+    User.create! @attr
+  end
+
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+    #no_name_user.valid?.should_not == true
+  end
+
+  it "should require a email" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com THE_USER@foo.com first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.shoud be_valid
+    end
+  end
+end
