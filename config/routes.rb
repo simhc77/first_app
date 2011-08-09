6d8126af673cc70d7c7bd06711a3bbe9dae359c9
simhc77 <alex.simhc@gmail.com>
2011-08-09 10:57:03 +0900

    aa

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 FirstApp::Application.routes.draw do
 
-  get '/users/new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -47,6 +46,7 @@ FirstApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :users
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
